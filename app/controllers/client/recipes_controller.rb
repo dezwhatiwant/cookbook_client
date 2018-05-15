8b825ca6aa7725e43711f16062537b1a9dce2b04
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-14 20:54:16 -0500

    added edit and update client actions

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -16,13 +16,14 @@ class Client::RecipesController < ApplicationController
                      chef: params[:chef],
                      ingredients: params[:ingredients],
                      directions: params[:directions],
-                     prep_time: params[:prep_time]
+                     prep_time: params[:prep_time],
+                     image_url: params[:image_url]
                     }
     response = Unirest.post(
                             "http://localhost:3000/api/recipes",
                             parameters: client_params
                             )
-    
+
     render 'create.html.erb'
   end
 
@@ -31,6 +32,31 @@ class Client::RecipesController < ApplicationController
     response = Unirest.get("http://localhost:3000/api/recipes/#{recipe_id}")
     @recipe = response.body
     render 'show.html.erb'
+    
+  end
+
+  def edit
+    recipe_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/recipes/#{recipe_id}")
+    @recipe = response.body
+    render 'edit.html.erb'
+  end
+
+  def update
+   client_params = {
+                 title: params[:title],
+                 chef: params[:chef],
+                 ingredients: params[:ingredients],
+                 directions: params[:directions],
+                 prep_time: params[:prep_time],
+                 image_url: params[:image_url]
+                }
+
+  response = Unirest.patch(
+                          "http://localhost:3000/api/recipes/#{ params[:id] }",
+                          parameters: client_params
+                          )
+  render 'update.html.erb'
     
   end
 
